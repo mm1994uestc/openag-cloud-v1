63cf367488ee1043a2609cfec9b55b68ce9cc1cc
Rob Baynes <rbaynes@media.mit.edu>
2019-04-11 16:26:55 -0400

    add function to accept posted binary image

--- backend-iot-firebase-function-service/README.md
+++ backend-iot-firebase-function-service/README.md
@@ -5,11 +5,15 @@ What this function does:
 - validates the JSON against the document DB schema
 - inserts the document into our Firestore 'devicePublicKeys' collection
 
+## For the IoT registration POST from the device, See the brain script:
+`scripts/iot/one_time_key_creation_and_iot_device_registration.sh`
 
-## See the device-registration dir for the POST from the device.
+## For the image file POST from the device, See the brain script:
+`scripts/iot/send_image_test.sh`
 
 ## Setup Notes
 - If you see this error: `Error: The Cloud Firestore API is not enabled for the project fb-func-test`
 - Then go to https://console.firebase.google.com
   - Develop > Database and enable "Cloud Firestore beta".
+
 
--- backend-iot-firebase-function-service/functions/src/index.ts
+++ backend-iot-firebase-function-service/functions/src/index.ts
@@ -1,20 +1,26 @@
 // Firebase cloud TypeScript (compiled to JS) function to
 // recieve a POSTed public key from a device and insert the key into
 // the Firestore document DB.
-//
+
 // For testing a local server:
 // curl http://localhost:5000/fb-func-test/us-central1/saveKey  -H "Content-Type: application/json" -X POST --data '{"key": "rob", "cksum": "1", "MAC":"and cheese", "timestamp": "2018-12-04T15:52:52Z"}'
-//
+
 // For testing a deployed server:
 // curl https://us-central1-fb-func-test.cloudfunctions.net/saveKey  -H "Content-Type: application/json" -X POST --data '{"key": "rob", "cksum": "1", "MAC":"and cheese", "timestamp": "2018-12-04T15:52:52Z"}'
+
+// console:
+// https://console.firebase.google.com/u/1/project/fb-func-test/functions/list
 
 
 'use strict';
 
 import * as functions from 'firebase-functions'
-const admin = require('firebase-admin');
+const firebase = require('firebase-admin');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
-const schema = {
+const key_schema = {
   "properties": {
     "key": { "type": "string" },
     "cksum": { "type": "string" },
@@ -22,49 +28,158 @@ const schema = {
     "MAC": { "type": "string" },
     "timestamp": { "type": "string" }
   },
-  "required": ["key", "cksum", "MAC"],
+  "required": ["key", "cksum", "MAC", "timestamp"],
   "additionalProperties": false
 };
-// debugrob: later after all new brain code is deployed, make timestamp required.   Sometime in Feb. 2019.
 
 const Ajv = require('ajv');
 let ajv = new Ajv({allErrors: true});
-const validate = ajv.compile( schema )
+const key_validate = ajv.compile( key_schema )
 
 
-admin.initializeApp({
-    credential: admin.credential.applicationDefault()
+firebase.initializeApp({
+    credential: firebase.credential.applicationDefault()
 });
 
-// our doc DB collection 
-var db = admin.firestore();
-var objectsRef = db.collection('devicePublicKeys');
+// view both in the firebase console 
+// https://console.firebase.google.com/u/1/project/fb-func-test
 
-// firebase cloud function
+// our doc DB collection 
+var db = firebase.firestore();
+var keys_db_ref = db.collection('devicePublicKeys');
+var images_db_ref = db.collection('deviceUploadedImages');
+
+// our file storage for images
+var storage = firebase.storage();
+
+// we are saving images to our openag-v1 google storage bucket with the name
+// below.   view the bucket with this link:
+// https://console.cloud.google.com/storage/browser/openag-public-image-uploads?project=openag-v1&organizationId=748412295656
+//
+const BUCKET_NAME = 'openag-public-image-uploads';
+const BUCKET = 'gs://' + BUCKET_NAME;
+const BUCKET_URL = 'https://storage.googleapis.com/' + BUCKET_NAME + '/';
+
+
+//-----------------------------------------------------------------------------
+// firebase cloud function to save an RSA public key
 export const saveKey = functions.https.onRequest((request, response) => {
     if( request.method != 'POST' ) {
-        response.status(403).json({error: 
-            "invalid HTTP method, only POST allowed"});
+        response.status(403).json({error: "nope"}); // only POST allowed
     }
 
     // this uses the JSON data POSTed in the URL
     let doc = request.body;
     //console.log( 'Received:', doc );
-    let isvalid = validate( doc );
+    let isvalid = key_validate( doc );
 
     // add the state property
     doc.state = "unclaimed";
 
     // add doc to the DB
     if( isvalid ) {
-        objectsRef.add( doc ).then( newdoc => {
-            console.log('Added document with ID: ', newdoc.id);
-            response.json({ok: newdoc.id});
+        keys_db_ref.add( doc ).then( newdoc => {
+            //console.log('Added document with ID: ', newdoc.id);
+            response.json({ok: 'ok'});
         });
     } else {
-        console.log( 'Invalid:' + ajv.errorsText(validate.errors));
-        response.status(422).json({error: "Validation Failed: " + 
-            ajv.errorsText(validate.errors)}); 
+        console.log( 'Invalid:' + ajv.errorsText(key_validate.errors));
+        response.status(403).json({error: "nope"}); 
     }
+});
+
+
+//-----------------------------------------------------------------------------
+// firebase cloud function to save an image
+export const saveImage = functions.https.onRequest((request, response) => {
+    if( request.method != 'POST' ) {
+        response.status(403).json({error: "nope"}); 
+    }
+
+    // this uses the raw file data POSTed in the URL
+    //console.log( 'request:', request );
+    //console.log( 'request.body.length:', request.body.length );
+    let file_contents = request.body;
+
+    /* must remove the first bit of the uploade data, it is a header:
+    --------------------------6717bd8e17e47c61^M
+    Content-Disposition: form-data; name="data"; filename="EDU-43E028C7-c4-b3-01-8d-9b-8c"^M
+    Content-Type: application/octet-stream^M
+    ^M
+    */
+
+    let offset = 0; 
+    while( offset < file_contents.length ) {
+        let four_bytes = file_contents.readUInt32BE(offset);
+        let four_byte_hex_str = four_bytes.toString(16);
+        if( four_byte_hex_str == 'd0a0d0a' ) {
+            offset += 4;
+            break;
+        }
+        offset++;
+    }
+    //console.log( 'offset', offset );
+
+    let header = String('');
+    for( var i=0; i<offset; i++ ) {
+        let h = file_contents.readUInt8(i);
+        header += String.fromCharCode(h);
+    }
+    //console.log( 'header', header );
+
+    // parse the device ID from the header (in the filename field)
+    let fn = header.indexOf("filename=");
+    let did_index_start = fn + "filename=\"".length;
+    let did_index_end = header.indexOf("\"", did_index_start);
+    let did = header.substring(did_index_start, did_index_end);
+    //console.log( 'device_id', did );
+
+    // remove header and overwrite file contents Buffer
+    file_contents = file_contents.slice(offset); 
+    //console.log( 'new buffer length:', file_contents.length );
+
+    // get UTC timestamp
+    let TS = new Date().toISOString();
+
+    // make a unique file name from UTC + DeviceID 
+    let file_name = did + '_' + TS + '.png';
+
+    // save the file to gstorage
+    // https://cloud.google.com/nodejs/docs/reference/storage/2.3.x/Bucket#file
+    let bucket = storage.bucket(BUCKET); // default firebase storage bucket
+    let file = bucket.file(file_name);
+    const metadata = {
+        contentType: 'image/png',
+        metadata: {
+            device_id: did,
+            timestamp: TS
+        }
+    };
+    file.save(file_contents, metadata, function(err) {
+        if(err) {
+            console.log("Error saving image to gstorage:", file_name);
+            response.status(403).json({error: "nope"}); 
+        }
+    });
+    //console.log("Saved image to gstorage:", file_name);
+
+    // also save the fact we wrote a file to storate, to firebase doc DB
+    const doc = {
+        bucket: BUCKET,
+        file_name: file_name,
+        URL: BUCKET_URL + file_name,
+        device_id: did,
+        timestamp: TS
+    };
+    images_db_ref.add( doc ).then( newdoc => {
+        //console.log('Added image meta doc: ', doc);
+    })
+    .catch(err => {
+        console.log("Error saving metadata to doc db:", doc);
+        response.status(403).json({error: "nope"}); 
+    });
+
+    // done OK if we get here
+    response.json({ok: 'ok'});
 });
 
